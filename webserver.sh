c97880b542da04996d7427f713607b8422ea0d54
dharu-cloud <dharanicharu99@gmail.com>
2021-04-23 11:00:03 +0000

    adding the new change

--- webserver.sh
+++ webserver.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 yum install -y httpd
-echo "<hi>This ia a webpage which has been build with the help of jenkins server <h1>" /var/www/html/index.html
+echo "<h1>This ia a webpage which has been build with the help of jenkins server <h1>" /var/www/html/index.html
 systemctl start httpd
 systemctl status httpd
 sleep 10
